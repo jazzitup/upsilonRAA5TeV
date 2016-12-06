2f836f5e7cbd5208817b1d2caea0a0de3f7e958e
JaeBeom Park <pjwinnetou@gmail.com>
2016-12-06 18:15:01 +0900

    new weight info

--- compareDataMc/compareSpectra.C
+++ compareDataMc/compareSpectra.C
@@ -123,23 +123,34 @@ void compareSpectra(int state = 1, int collId= kPPDATA) {
 
 
   if(state==1) funct = new TF1("dataMcRatio",fTsallis1SR, 0,30,4);
-  else if(state==2) funct = new TF1("dataMcRatio","[0]*x+[1]", 0,30);
-  //else if(state==2) funct = new TF1("dataMcRatio",fTsallis2SR, 0,30,4);
+  //else if(state==2) funct = new TF1("dataMcRatio","[0]*x+[1]", 0,30);
+  else if(state==2) funct = new TF1("dataMcRatio",fTsallis2SR, 0,30,4);
 
-  if(state==1){
-  funct->SetParameters(5,5,5,5);
-  funct->SetParLimits(0,0.,120);
-  funct->SetParLimits(1,0.,100);
-  funct->SetParLimits(2,0.,150);
-  funct->SetParLimits(3,0.,100);
-  }
-  else if(state==2){
-  funct->SetParameters(5,5);
-  funct->SetParLimits(0,-10.,10);
-  funct->SetParLimits(1,-10.,10);
-  }
-  hRatio->Fit ( funct, "R");
+  //PP 1S
+/*  funct->SetParameters(0.06123,1.023,2.123,1);
+  funct->SetParLimits(0,-1.,1.3);
+  funct->SetParLimits(1, 0.001,3.1);
+  funct->SetParLimits(2,0.12,6);
+  funct->SetParLimits(3,-1.,20);
+  */
 
+  //AA 1S
+  
+  funct->SetParameters(0.06123,1.023,2.123,1);
+  funct->SetParLimits(0,-1.,4.3);
+  funct->SetParLimits(1, 0.00,5.1);
+  funct->SetParLimits(2,0.1,6);
+  funct->SetParLimits(3,-1.,20);
+  
+
+  //2S
+ /* funct->SetParameters(5,5,5,5);
+  funct->SetParLimits(0,0.,150.3);
+  funct->SetParLimits(1, 0.0,150.1);
+  funct->SetParLimits(2,0.0,150);
+  funct->SetParLimits(3,0.,150);
+  */
+  hRatio->Fit ( funct, "REM");
   hRatio->Draw();
   jumSun(0,1,30,1);
 
@@ -155,10 +166,11 @@ void compareSpectra(int state = 1, int collId= kPPDATA) {
   }
   else if(state==2)
   {
+    //cout << "TF1* weightFunction = new TF1(@weightCurve@,@[0]*x+[1]);" << endl;   cout << " weightFunction->SetParameters( " ;
     cout << "TF1* weightFunction = new TF1(@weightCurve@,@(([0]-1)*([0]-2)*([2]*[3]*([2]*[3]+([2]-2)*10.023))*TMath::Power((1+(TMath::Sqrt(10.023*10.023+x*x)-10.023)/([0]*[1])),-[0])/(([0]*[1]*([0]*[1]+([0]-2)*10.023))*(([2]-1)*([2]-2))*TMath::Power((1+(TMath::Sqrt(10.032*10.023+x*x)-10.023)/([2]*[3])),-[2]))));" << endl;   cout << " weightFunction->SetParameters( " ;
-    for ( int i=0 ; i<2 ; i++) 
+    for ( int i=0 ; i<4 ; i++) 
     { 
-      if ( i!=1 ) 
+      if ( i!=3 ) 
         cout << funct->GetParameter(i) << ", " ;
       else   cout << funct->GetParameter(i) << "); "<< endl ;
     }
@@ -173,20 +185,13 @@ void compareSpectra(int state = 1, int collId= kPPDATA) {
   TF1 *funct_write;
 
   if(state==1) funct_write = new TF1(Form("f_%s_1sstate",fcollId.Data()),"([0]-1)*([0]-2)*([2]*[3]*([2]*[3]+([2]-2)*9.460))*TMath::Power((1+(TMath::Sqrt(9.460*9.460+x*x)-9.460)/([0]*[1])),-[0])/(([0]*[1]*([0]*[1]+([0]-2)*9.460))*(([2]-1)*([2]-2))*TMath::Power((1+(TMath::Sqrt(9.460*9.460+x*x)-9.460)/([2]*[3])),-[2]))",0,30);
-  else if(state==2) funct_write = new TF1(Form("f_%s_2sstate",fcollId.Data()),"[0]*x+[1]",0,30);
-  //else if(state==2) funct_write = new TF1(Form("f_%s_2sstate",fcollId.Data()),"([0]-1)*([0]-2)*([2]*[3]*([2]*[3]+([2]-2)*10.023))*TMath::Power((1+(TMath::Sqrt(10.023*10.023+x*x)-10.023)/([0]*[1])),-[0])/(([0]*[1]*([0]*[1]+([0]-2)*10.023))*(([2]-1)*([2]-2))*TMath::Power((1+(TMath::Sqrt(10.032*10.023+x*x)-10.023)/([2]*[3])),-[2]))",0,30);
+  //else if(state==2) funct_write = new TF1(Form("f_%s_2sstate",fcollId.Data()),"[0]*x+[1]",0,30);
+  else if(state==2) funct_write = new TF1(Form("f_%s_2sstate",fcollId.Data()),"([0]-1)*([0]-2)*([2]*[3]*([2]*[3]+([2]-2)*10.023))*TMath::Power((1+(TMath::Sqrt(10.023*10.023+x*x)-10.023)/([0]*[1])),-[0])/(([0]*[1]*([0]*[1]+([0]-2)*10.023))*(([2]-1)*([2]-2))*TMath::Power((1+(TMath::Sqrt(10.032*10.023+x*x)-10.023)/([2]*[3])),-[2]))",0,30);
 
-  if(state ==1){
   funct_write -> FixParameter(0,funct->GetParameter(0));
   funct_write -> FixParameter(1,funct->GetParameter(1));
   funct_write -> FixParameter(2,funct->GetParameter(2));
   funct_write -> FixParameter(3,funct->GetParameter(3));
-  }
-
-  if(state==2){
-  funct_write -> FixParameter(0,funct->GetParameter(0));
-  funct_write -> FixParameter(1,funct->GetParameter(1));
-  }
 
   c1->SaveAs(Form("dNdpT_dataMC_%s_DATA_%dsState.png",fcollId.Data(),state));
   TFile* outf = new TFile(Form("ratioDataMC_%s_%dsState.root",getCollID(collId).Data(), state),"recreate");
@@ -206,7 +211,7 @@ valErr getYield(int state, int collId, float ptLow, float ptHigh, float yLow, fl
     float dphiEp2Low,  float dphiEp2High) {
   TString kineLabel = getKineLabel (collId, ptLow, ptHigh, yLow, yHigh, glbMuPtCut, cLow, cHigh, dphiEp2Low, dphiEp2High) ;
   TString SignalCB = "Double";
-  TFile* inf = new TFile(Form("../../TESTPLOT_FORNOMINAL/fitresults_upsilon_%sCB_%s.root",SignalCB.Data(),kineLabel.Data()));
+  TFile* inf = new TFile(Form("../fitResults/nominalFits/fitresults_upsilon_%sCB_%s.root",SignalCB.Data(),kineLabel.Data()));
 //TFile* inf = new TFile(Form("../fitResults/dataFit_fixParam1MuPt4_2016_08_30/fitresults_upsilon_%sCB_%s.root",SignalCB.Data(),kineLabel.Data()));
   TH1D* fitResults = (TH1D*)inf->Get("fitResults");
   valErr ret; 
