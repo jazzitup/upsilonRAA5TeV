50d47df2cf61d34f6e375986013ba21ca2cd51c5
JaeBeom Park <pjwinnetou@gmail.com>
2016-12-08 10:53:32 +0900

    bkg code

--- Systematic/computeSysUncrt_bkgPDF.C
+++ Systematic/computeSysUncrt_bkgPDF.C
@@ -42,6 +42,21 @@ void computeSysUncrt_bkgPDF(int state=2)
     nCentBins = nCentBins3s;  centBin = centBin3s;
     nYBins = nYBins3S;  yBin = yBin3S;
   }
+  
+  TH1D *hUncPtPP = new TH1D("hptPP",";Uncertainty;p_{T} (GeV/c)",nPtBins,ptBin);
+  TH1D *hUncRapPP = new TH1D("hrapPP",";Uncertainty;|y|",nYBins,yBin);
+  TH1D *hUncIntPP = new TH1D("hIntPP",";Uncertainty;Centrality",1,0,200);
+  
+  TH1D *hUncCentAA = new TH1D("hcentAA","; Uncertainty ; Centrality Bins", nCentBins, centBin);
+  TH1D *hUncPtAA = new TH1D("hptAA",";Uncertainty;p_{T} (GeV/c)",nPtBins,ptBin);
+  TH1D *hUncRapAA = new TH1D("hrapAA",";Uncertainty;|y|",nYBins,yBin);
+  TH1D *hUncIntAA = new TH1D("hIntAA",";Uncertainty;Centrality",1,0,200);
+  
+  TH1D *hUncIntAAoPP = new TH1D("hIntAAoPP",";Uncertainty;Centrality",1,0,200);
+  TH1D *hUncCentAAoPP = new TH1D("hcentAAoPP","; Uncertainty ; Centrality Bins", nCentBins, centBin);
+  TH1D *hUncPtAAoPP = new TH1D("hptAAoPP",";Uncertainty;p_{T} (GeV/c)",nPtBins,ptBin);
+  TH1D *hUncRapAAoPP = new TH1D("hrapAAoPP",";Uncertainty;|y|",nYBins,yBin);
+
 
   cout << "state : " << state << endl;
   // centrality dependence :
@@ -49,19 +64,30 @@ void computeSysUncrt_bkgPDF(int state=2)
   cout << " CENTRALITY DEPENDENCE===================== " << endl;
   cout << " pp :   " << endl; 
   pp = getRMS("fitresults_upsilon_DoubleCB_PP_DATA_pt0.0-30.0_y0.0-2.4_muPt4.0",state); // pp.  pT/y integrated
+  hUncIntPP -> SetBinContent(1,pp);
   cout << " AA :   " << endl; 
   for ( int icent = 0 ; icent<= nCentBins ; icent++ ) 
   {
     cout << " icent : " << icent << endl;
     if(icent == 0) cout << "Centrality 0 - 200" << endl;
     else cout << Form("Centrality  %.0f - %.0f", centBin[icent-1], centBin[icent]) << endl;
-    if(icent == 0) aa = getRMS("fitresults_upsilon_DoubleCB_AA_DATA_pt0.0-30.0_y0.0-2.4_muPt4.0_centrality0-200_dphiEp_0.00PI_100.00PI",state);
+    if(icent == 0){ 
+      aa = getRMS("fitresults_upsilon_DoubleCB_AA_DATA_pt0.0-30.0_y0.0-2.4_muPt4.0_centrality0-200_dphiEp_0.00PI_100.00PI",state);
+    }
     else 
     {
       if(centBin[icent] <= 60) aa = getRMS(Form("fitresults_upsilon_DoubleCB_AA_DATA_pt0.0-30.0_y0.0-2.4_muPt4.0_centrality%.0f-%.0f_dphiEp_0.00PI_100.00PI", centBin[icent-1], centBin[icent]),state );
       else if(centBin[icent] > 60) aa = getRMS(Form("fitresults_upsilon_DoubleCB_AA_DATA_pt0.0-30.0_y0.0-2.4_muPt4.0_centrality%.0f-%.0f_dphiEp_0.00PI_100.00PI", centBin[icent-1], centBin[icent]),state );
     }
     finalUnc = getFinalUnc(pp,aa);
+    if(icent == 0) {
+      hUncIntAA -> SetBinContent(1,aa);
+      hUncIntAAoPP -> SetBinContent(1,finalUnc);
+    } 
+    else if(icent != 0) {
+      hUncCentAA -> SetBinContent(icent, aa);
+      hUncCentAAoPP -> SetBinContent(icent,finalUnc);
+    }
     cout << "Unc. of double ratio  " << 0.00001* int(10000000*finalUnc) << "%%" << endl;
     cout << endl;
   }
@@ -75,6 +101,9 @@ void computeSysUncrt_bkgPDF(int state=2)
     pp = getRMS(Form("fitresults_upsilon_DoubleCB_PP_DATA_pt%.1f-%.1f_y0.0-2.4_muPt4.0", ptBin[ipt-1], ptBin[ipt]), state); // pp
     aa = getRMS(Form("fitresults_upsilon_DoubleCB_AA_DATA_pt%.1f-%.1f_y0.0-2.4_muPt4.0_centrality0-200_dphiEp_0.00PI_100.00PI", ptBin[ipt-1], ptBin[ipt] ),state ); // PbPb
     finalUnc = getFinalUnc(pp,aa);
+    hUncPtPP->SetBinContent(ipt,pp);
+    hUncPtAA->SetBinContent(ipt,aa);
+    hUncPtAAoPP->SetBinContent(ipt,finalUnc);
     cout << "Unc. of double ratio  " << 1.000000* double(100*finalUnc) << "%%" << endl;
     cout << endl;
   }
@@ -88,13 +117,26 @@ void computeSysUncrt_bkgPDF(int state=2)
     pp = getRMS(Form("fitresults_upsilon_DoubleCB_PP_DATA_pt0.0-30.0_y%.1f-%.1f_muPt4.0", yBin[iy-1], yBin[iy]),state ); // pp
     aa = getRMS(Form("fitresults_upsilon_DoubleCB_AA_DATA_pt0.0-30.0_y%.1f-%.1f_muPt4.0_centrality0-200_dphiEp_0.00PI_100.00PI", yBin[iy-1], yBin[iy]),state ); // PbPb
     finalUnc = getFinalUnc(pp,aa);
+    hUncRapPP->SetBinContent(iy,pp);
+    hUncRapAA->SetBinContent(iy,aa);
+    hUncRapAAoPP->SetBinContent(iy,finalUnc);
     cout << "Unc. of double ratio  " << 1.000000* double(100*finalUnc) << "%%" << endl;
   }
 
-//  hcentGenAA = new TH1D("hcentGenAA","; centrality bin ; ", nCentBins, centBin);
-//  TH1D *hUncPtPP = new TH1D("hUncPtAA",";Uncertainty;p_{T} (GeV/c
+  TFile *wf = new TFile(Form("sys_bkgPDFVariaion_%ds.root",state),"recreate");
 
-
+  wf->cd();
+  hUncRapPP->Write();
+  hUncRapAA->Write();
+  hUncRapAAoPP->Write();
+  hUncPtPP->Write();
+  hUncPtAA->Write();
+  hUncPtAAoPP->Write();
+  hUncIntAA->Write();
+  hUncIntPP->Write();
+  hUncIntAAoPP->Write();
+  hUncCentAA->Write();
+  hUncCentAAoPP->Write();
 
 }
 
@@ -108,7 +150,7 @@ double getRMS(TString fileName , int state)
   cout << "Var1 - Var5 : " ;
   for ( int ivar=0 ; ivar<=nVar ; ivar++) 
   { 
-    if(ivar ==0) fin = Form("../../plots/TESTPLOT_FORNOMINAL/%s.root",fileName.Data());
+    if(ivar ==0) fin = Form("/home/samba/UpsilonAnalysis/fitResultFiles/mcFit_MuPt4_2016_11_04/%s.root",fileName.Data());
     else fin = Form("BkgVariation/Sys_BkgVar_%s.root", fileName.Data());
     TFile* f1 = new TFile(fin.Data() );
     TH1D* h = (TH1D*)f1->Get("fitResults");
