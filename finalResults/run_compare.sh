ea9506a869ea5777843f6a439016437ce9f6e394
SongkyoLee <songkyo.lee@cern.ch>
2016-12-13 15:59:55 +0900

    run the macros

--- finalResults/run_compare.sh
+++ finalResults/run_compare.sh
@@ -1,10 +1,10 @@
-root -l -b -q 'compare_15001_RAA_pt.C+(1)'
-root -l -b -q 'compare_15001_RAA_pt.C+(2)'
+root -l -b -q 'compare_15001_RAA_pt.C+(1)' #1S
+root -l -b -q 'compare_15001_RAA_pt.C+(2)' #2S
 root -l -b -q 'compare_15001_RAA_rap.C+(1)'
 root -l -b -q 'compare_15001_RAA_rap.C+(2)'
-root -l -b -q 'compare_15001_RAA_cent_noint.C+(1)'
-root -l -b -q 'compare_15001_RAA_cent_noint.C+(2)'
 root -l -b -q 'compare_15001_RAA_cent.C+(1)'
 root -l -b -q 'compare_15001_RAA_cent.C+(2)'
+#root -l -b -q 'compare_15001_RAA_cent_noint.C+(1)'
+#root -l -b -q 'compare_15001_RAA_cent_noint.C+(2)'
 
 rm -f *.so *.d *.pcm
--- finalResults/run_draw_RAA.sh
+++ finalResults/run_draw_RAA.sh
@@ -1,6 +1,7 @@
 root -l -b -q draw_RAA_pt.C+
 root -l -b -q draw_RAA_rap.C+
-root -l -b -q draw_RAA_cent_noint.C+
-root -l -b -q draw_RAA_cent.C+
+root -l -b -q 'draw_RAA_cent.C+(1)' # upper limit for 3S (actual)
+#root -l -b -q 'draw_RAA_cent.C+(0)' # just point for 3S
+#root -l -b -q draw_RAA_cent_noint.C+
 
 rm -f *.so *.d *.pcm
