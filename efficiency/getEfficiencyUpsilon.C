1f77d72c5e53b97a01d97b3c426897c682dea896
Yongsun <kingmking@gmail.com>
2016-12-15 16:23:18 +0900

    changed the structure of macro..

--- efficiency/getEfficiencyUpsilon.C
+++ efficiency/getEfficiencyUpsilon.C
@@ -4,6 +4,7 @@
 #include "TFile.h"
 #include "../cutsAndBin.h"
 #include "../multiTreeUtil.h"
+#include "tnp_weight.h"
 using namespace std;
 
 
@@ -11,148 +12,220 @@ using namespace std;
 
 TLegend *leg = new TLegend(0.55,0.2, 0.85,0.4,NULL,"brNDC");
 
-void setupMultiTreeTool( multiTreeUtil* mt=0, int UpsState=kPPMCUps1S, bool isGen=true) ;
-void getEfficiencyUpsilon(int state = 1, bool useDataWeight=true) {  // 1S, 2S, 3S
-  
+
+
+
+void getEfficiencyUpsilon(int state = 1, bool useDataWeight=true, bool useTnpWeigt=false) {  // 1S, 2S, 3S
   TH1::SetDefaultSumw2();
-  //// modify by hand according to the pt range of the sample
-  TString weightString = "weight";
-  if (useDataWeight == false) {
-    weightString = "weight0";
-  }
 
+  float massLow = 8;
+  float massHigh = 13;
   
-  TCut accCut = Form("(pt1>%f) && (pt2>%f) && abs(eta1)<2.4 && abs(eta2)<2.4", (float)glbMuPtCut, (float)glbMuPtCut);
-  multiTreeUtil* genAA = new multiTreeUtil();
-  multiTreeUtil* recoAA = new multiTreeUtil();
-  multiTreeUtil* genPP = new multiTreeUtil();
-  multiTreeUtil* recoPP = new multiTreeUtil();
-
   int nPtBins=0;
   double* ptBin;
   int nCentBins=0;
   double* centBin;
   int nYBins=0;
   double *yBin;
-
-  if ( state == 1 ) { 
+  if ( state == 1 ) {
     nPtBins = nPtBins1s;    ptBin = ptBin1s;
     nYBins = nYBins1S;  yBin = yBin1S;
     nCentBins = nCentBins1s;  centBin = centBin1s;
-    setupMultiTreeTool(genAA, kAAMCUps1S, true);  // isGen = 1
-    setupMultiTreeTool(recoAA, kAAMCUps1S, false);  
-    setupMultiTreeTool(genPP, kPPMCUps1S, true);  // isGen = 1
-    setupMultiTreeTool(recoPP, kPPMCUps1S, false);  
   }
-  else if ( state == 2 ) { 
+  else if ( state == 2 ) {
     nPtBins = nPtBins2s;    ptBin = ptBin2s;
     nCentBins = nCentBins2s;  centBin = centBin2s;
     nYBins = nYBins2S;  yBin = yBin2S;
-    setupMultiTreeTool(genAA, kAAMCUps2S, true);  // isGen = 1
-    setupMultiTreeTool(recoAA, kAAMCUps2S, false);  
-    setupMultiTreeTool(genPP, kPPMCUps2S, true);  // isGen = 1
-    setupMultiTreeTool(recoPP, kPPMCUps2S, false);  
   }
-  else if ( state == 3 ) { 
+  else if ( state == 3 ) {
     nPtBins = nPtBins3s;    ptBin = ptBin3s;
     nCentBins = nCentBins3s;  centBin = centBin3s;
     nYBins = nYBins3S;  yBin = yBin3S;
-    setupMultiTreeTool(genAA, kAAMCUps3S, true);  // isGen = 1
-    setupMultiTreeTool(recoAA, kAAMCUps3S, false);  
-    setupMultiTreeTool(genPP, kPPMCUps3S, true);  // isGen = 1
-    setupMultiTreeTool(recoPP, kPPMCUps3S, false);  
   }
+
+  
+  double ptMin = ptBin[0];    double ptMax = ptBin[nPtBins];
+  double yMin = yBin[0];    double yMax = yBin[nYBins];
+
+
 
 
   TH1D* hptGenAA;
   TH1D* hptRecoAA;
   TH1D* hptGenPP;
   TH1D* hptRecoPP;
-  
+
   TH1D* hrapGenAA;
   TH1D* hrapRecoAA;
   TH1D* hrapGenPP;
   TH1D* hrapRecoPP;
-  
+
   TH1D* hcentGenAA;
   TH1D* hcentRecoAA;
-  TH1D* hcentGenPP;
-  TH1D* hcentRecoPP;
 
   TH1D* hcentintGenAA;
   TH1D* hcentintRecoAA;
   TH1D* hcentintGenPP;
   TH1D* hcentintRecoPP;
-  
-  bool doCent = true; 
-  TCut PtCut = " pt >= 0 && pt < 30 ";
-  TCut CbinCut = " cBin >= 0 && cBin <= 200 ";
-  TCut yCut = " abs(y) >=0 && abs(y) < 2.4 ";
-
-  
   //*~*~*~* for integrated bins *~*~*~*
 
   hcentintGenAA = new TH1D("hcentintGenAA","",1,0,200);
   hcentintRecoAA = (TH1D*) hcentintGenAA->Clone("hcentintRecoAA");
   hcentintGenPP = (TH1D*) hcentintGenAA->Clone("hcentintGenPP");
   hcentintRecoPP = (TH1D*) hcentintGenAA->Clone("hcentintRecoPP");
-
-  genAA->Draw2(hcentintGenAA,"cBin", accCut && PtCut && CbinCut && yCut,weightString.Data());
-  recoAA->Draw2(hcentintRecoAA,"cBin", accCut && PtCut && CbinCut && yCut,weightString.Data());
-  genPP->Draw2(hcentintGenPP,"cBin", accCut && PtCut && CbinCut && yCut,weightString.Data());
-  recoPP->Draw2(hcentintRecoPP,"cBin", accCut && PtCut && CbinCut && yCut,weightString.Data());
-
-  
   //*~*~*~* for pt bins *~*~*~*
-  
   hptGenAA = new TH1D("hptGenAA","; p_{T} (GeV/c) ; ", nPtBins, ptBin);
   hptRecoAA = (TH1D*)hptGenAA->Clone("hptRecoAA");
   hptGenPP = (TH1D*)hptGenAA->Clone("hptGenPP");
   hptRecoPP = (TH1D*)hptGenAA->Clone("hptRecoPP");
-
-  genAA->Draw2( hptGenAA, "pt", accCut && yCut && CbinCut, weightString.Data() ) ;
-  recoAA->Draw2( hptRecoAA, "pt", accCut && yCut && CbinCut,weightString.Data() ) ;
-  genPP->Draw2( hptGenPP, "pt", accCut && yCut && CbinCut,weightString.Data()) ;
-  recoPP->Draw2( hptRecoPP, "pt", accCut && yCut && CbinCut,weightString.Data()) ;
-
-
-  //*~*~*~* for rapidity bins *~*~*~* 
-
+  //*~*~*~* for rapidity bins *~*~*~*
   hrapGenAA = new TH1D("hrapGenAA","; |y| ; ", nYBins, yBin);
   hrapRecoAA = (TH1D*)hrapGenAA->Clone("hrapRecoAA");
   hrapGenPP = (TH1D*)hrapGenAA->Clone("hrapGenPP");
   hrapRecoPP = (TH1D*)hrapGenAA->Clone("hrapRecoPP");
+  //*~*~*~* for centrality bins *~*~*~*
+  hcentGenAA = new TH1D("hcentGenAA","; centrality bin ; ", nCentBins, centBin);
+  hcentRecoAA = (TH1D*)hcentGenAA->Clone("hcentRecoAA");
 
-  genAA->Draw2( hrapGenAA, "y",accCut && PtCut && CbinCut, weightString.Data());
-  recoAA->Draw2( hrapRecoAA, "y",accCut && PtCut && CbinCut, weightString.Data());
-  genPP->Draw2( hrapGenPP, "y",accCut && PtCut && CbinCut, weightString.Data());
-  recoPP->Draw2( hrapRecoPP, "y",accCut && PtCut && CbinCut, weightString.Data());
-
-
-  //*~*~*~* for centrality bins *~*~*~* 
-  if ( doCent ) 
-  { 
-    hcentGenAA = new TH1D("hcentGenAA","; centrality bin ; ", nCentBins, centBin);
-    hcentRecoAA = (TH1D*)hcentGenAA->Clone("hcentRecoAA");
-    hcentGenPP = (TH1D*)hcentintGenAA->Clone("hcentGenPP");
-    hcentRecoPP = (TH1D*)hcentintGenAA->Clone("hcentRecoPP");
-
-    genAA->Draw2( hcentGenAA, "cBin", accCut && yCut && PtCut, weightString.Data() ) ;
-    recoAA->Draw2( hcentRecoAA, "cBin", accCut && yCut && PtCut,weightString.Data() ) ;
-
-    genPP->Draw2( hcentGenPP, "cBin", accCut && yCut && PtCut, weightString.Data() ) ;
-    recoPP->Draw2( hcentRecoPP, "cBin", accCut && yCut && PtCut,weightString.Data() ) ;
-  }
-
-
-  // *************************
-  // ****** Gen vs Reco ******
-  // *************************
   
+  TString fnameAA;
+  TString fnamePP;
+  if ( state == 1) { 
+    fnamePP = "../skimmedFilesWeight2/yskimPP_MC_Ups1S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20161281226_.root"; 
+    fnameAA = "../skimmedFilesWeight2/yskimAA_MC_Ups1S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20161281233_.root";  
+  }
+  else if ( state == 2 ) { 
+    fnamePP = "../skimmedFilesWeight2/yskimPP_MC_Ups2S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20161281228_.root";
+    fnameAA = "../skimmedFilesWeight2/yskimAA_MC_Ups2S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20161281234_.root";
+  }
+  else if ( state == 3 ) { 
+    fnamePP = "../skimmedFilesWeight2/yskimPP_MC_Ups3S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20161281230_.root"; 
+    fnameAA = "../skimmedFilesWeight2/yskimAA_MC_Ups3S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20161281235_.root"; 
+  }
+  
+  TChain *mmAA = new TChain("mm");
+  mmAA->Add(fnameAA);
+  
+  DiMuon dmAA;
+  TBranch        *b_dmAA;
+  mmAA->SetBranchAddress("mm",&dmAA,&b_dmAA);
+  
+  for(int iev=0; iev<mmAA->GetEntries() ; ++iev)  
+    {
+      mmAA->GetEntry(iev);
+      //Accpetnace 
+      if (  !( (dmAA.pt1 > glbMuPtCut) && (dmAA.pt2 > glbMuPtCut) && ( fabs(dmAA.eta1) < 2.4 ) &&  ( fabs(dmAA.eta2) <2.4 ) ) )
+	continue;
+      
+      if (  !( (dmAA.mass > massLow) 
+	       && (dmAA.mass < massHigh) 
+	       && ( dmAA.pt > ptMin) 
+	       && ( dmAA.pt < ptMax) 
+	       && ( fabs(dmAA.y) > yMin)
+	       && ( fabs(dmAA.y) < yMax)	      )
+	    )
+	continue;
+      
+      hcentintRecoAA->Fill( dmAA.cBin, dmAA.weight);
+      hcentRecoAA->Fill   ( dmAA.cBin, dmAA.weight);
+      hptRecoAA->Fill     ( dmAA.pt,   dmAA.weight);
+      hrapRecoAA->Fill    ( dmAA.y,    dmAA.weight);
+    }
+  
+  
+  TChain *mmGenAA = new TChain("mmGen");   
+  mmGenAA->Add(fnameAA);
+  DiMuon dmGenAA; 
+  TBranch        *b_dmGenAA;
+  mmGenAA->SetBranchAddress("mmGen",&dmGenAA,&b_dmGenAA);
+  
+  for(int iev=0; iev<mmGenAA->GetEntries() ; ++iev)  
+    {
+      mmGenAA->GetEntry(iev);
+      //Accpetnace 
+      if (  !( (dmGenAA.pt1 > glbMuPtCut) && (dmGenAA.pt2 > glbMuPtCut) && ( fabs(dmGenAA.eta1) < 2.4 ) &&  ( fabs(dmGenAA.eta2) <2.4 ) ) )
+	continue;
+      
+      if (  !( (dmGenAA.mass > massLow) 
+	       && (dmGenAA.mass < massHigh) 
+	       && ( dmGenAA.pt > ptMin) 
+	       && ( dmGenAA.pt < ptMax) 
+	       && ( fabs(dmGenAA.y) > yMin)
+	       && ( fabs(dmGenAA.y) < yMax)	      )
+	    )
+	continue;
+      
+      hcentintGenAA->Fill( dmGenAA.cBin, dmGenAA.weight);
+      hcentGenAA->Fill   ( dmGenAA.cBin, dmGenAA.weight);
+      hptGenAA->Fill     ( dmGenAA.pt,   dmGenAA.weight);
+      hrapGenAA->Fill    ( dmGenAA.y,    dmGenAA.weight);
+    }
+
+  // pp
+
+  TChain *mmPP = new TChain("mm");
+  mmPP->Add(fnamePP);
+  DiMuon dmPP;
+  TBranch        *b_dmPP;
+  mmPP->SetBranchAddress("mm",&dmPP,&b_dmPP);
+  
+  for(int iev=0; iev<mmPP->GetEntries() ; ++iev)  
+    {
+      mmPP->GetEntry(iev);
+      //Accpetnace
+      if (  !( (dmPP.pt1 > glbMuPtCut) && (dmPP.pt2 > glbMuPtCut) && ( fabs(dmPP.eta1) < 2.4 ) &&  ( fabs(dmPP.eta2) <2.4 ) ) )
+        continue;
+
+      if (  !( (dmPP.mass > massLow)
+               && (dmPP.mass < massHigh)
+               && ( dmPP.pt > ptMin)
+               && ( dmPP.pt < ptMax)
+               && ( fabs(dmPP.y) > yMin)
+               && ( fabs(dmPP.y) < yMax)              )
+            )
+        continue;
+
+      hcentintRecoPP->Fill( dmPP.cBin, dmPP.weight);
+      hptRecoPP->Fill     ( dmPP.pt,   dmPP.weight);
+      hrapRecoPP->Fill    ( dmPP.y,    dmPP.weight);
+    }
+  
+  
+  TChain *mmGenPP = new TChain("mmGen");
+  mmGenPP->Add(fnamePP);
+  
+  DiMuon dmGenPP;
+  TBranch        *b_dmGenPP;
+  mmGenPP->SetBranchAddress("mmGen",&dmGenPP,&b_dmGenPP);
+  
+  for(int iev=0; iev<mmGenPP->GetEntries() ; ++iev)  
+    {
+      mmGenPP->GetEntry(iev);
+      //Accpetnace
+      if (  !( (dmGenPP.pt1 > glbMuPtCut) && (dmGenPP.pt2 > glbMuPtCut) && ( fabs(dmGenPP.eta1) < 2.4 ) &&  ( fabs(dmGenPP.eta2) <2.4 ) ) )
+	continue;
+
+      if (  !( (dmGenPP.mass > massLow)
+               && (dmGenPP.mass < massHigh)
+               && ( dmGenPP.pt > ptMin)
+	       && ( dmGenPP.pt < ptMax)
+               && ( fabs(dmGenPP.y) > yMin)
+               && ( fabs(dmGenPP.y) < yMax)              )
+            )
+        continue;
+      
+      hcentintGenPP->Fill( dmGenPP.cBin, dmGenPP.weight);
+      hptGenPP->Fill     ( dmGenPP.pt,   dmGenPP.weight);
+      hrapGenPP->Fill    ( dmGenPP.y,    dmGenPP.weight);
+    }
+  
+
+  // DRAW! 
+
   // Pt Bins
   TCanvas* c_pt =  new TCanvas("c_pt","",600,600);
   c_pt->Divide(2,1);
-  
+
   c_pt->cd(1);
   handsomeTH1(hptGenAA,1);
   handsomeTH1(hptRecoAA,2);
@@ -162,7 +235,7 @@ void getEfficiencyUpsilon(int state = 1, bool useDataWeight=true) {  // 1S, 2S, 
   hptRecoAA->Draw("same");
   drawText("PbPb", 0.3, 0.8, 1, 15);
   drawText("|y| < 2.4", 0.3, 0.4, 1, 17);
-    
+
   c_pt->cd(2);
   handsomeTH1(hptGenPP,1);
   handsomeTH1(hptRecoPP,2);
@@ -172,11 +245,11 @@ void getEfficiencyUpsilon(int state = 1, bool useDataWeight=true) {  // 1S, 2S, 
   hptRecoPP->Draw("same");
   drawText("pp", 0.3, 0.8, 1, 15);
   drawText("|y| < 2.4", 0.3, 0.4, 1, 17);
-    
+
   // Rap Bins
   TCanvas* c_rap =  new TCanvas("c_rap","",600,600);
   c_rap->Divide(2,1);
-  
+
   c_rap->cd(1);
   handsomeTH1(hrapGenAA,1);
   handsomeTH1(hrapRecoAA,2);
@@ -186,7 +259,7 @@ void getEfficiencyUpsilon(int state = 1, bool useDataWeight=true) {  // 1S, 2S, 
   hrapRecoAA->Draw("same");
   drawText("PbPb", 0.3, 0.8, 1, 15);
   drawText("0 < p_{T} < 30 GeV/c", 0.3, 0.4, 1, 17);
-    
+
   c_rap->cd(2);
   handsomeTH1(hrapGenPP,1);
   handsomeTH1(hrapRecoPP,2);
@@ -196,13 +269,11 @@ void getEfficiencyUpsilon(int state = 1, bool useDataWeight=true) {  // 1S, 2S, 
   hrapRecoPP->Draw("same");
   drawText("pp", 0.3, 0.8, 1, 15);
   drawText("0 < p_{T} < 30 GeV/c", 0.3, 0.4, 1, 17);
-    
+
 
   // Centrality Bins
-  TCanvas* c_cent =  new TCanvas("c_cent","",600,600);
-  c_cent->Divide(2,1);
-  
-  c_cent->cd(1);
+  TCanvas* c_cent =  new TCanvas("c_cent","",400,400);
+
   handsomeTH1(hcentGenAA,1);
   handsomeTH1(hcentRecoAA,2);
   handsomeTH1(hcentintGenAA,1);
@@ -215,22 +286,7 @@ void getEfficiencyUpsilon(int state = 1, bool useDataWeight=true) {  // 1S, 2S, 
   hcentRecoAA->Draw("same");
   drawText("PbPb", 0.3, 0.8, 1, 15);
   drawText("|y| < 2.4", 0.3, 0.4, 1, 17);
-    
-  c_cent->cd(2);
-  handsomeTH1(hcentGenPP,1);
-  handsomeTH1(hcentRecoPP,2);
-  handsomeTH1(hcentintGenPP,1);
-  handsomeTH1(hcentintRecoPP,2);
-  cleverRange(hcentGenPP, 1.3, 0);
-  cleverRange(hcentintGenPP, 1.3, 0);
-  hcentGenPP->SetYTitle("dN/dN_{part}");
-  hcentintGenPP->SetYTitle("dN/dN_{part}");
-  hcentGenPP->Draw("hist");
-  hcentRecoPP->Draw("same");
-  drawText("pp", 0.3, 0.8, 1, 15);
-  drawText("|y| < 2.4", 0.3, 0.4, 1, 17);
-    
-  
+
   // *************************
   // ****** Efficiency *******
   // *************************
@@ -256,9 +312,9 @@ void getEfficiencyUpsilon(int state = 1, bool useDataWeight=true) {  // 1S, 2S, 
   leg2->AddEntry(hptEffAA, "PbPb (0-100%)");
   leg2->AddEntry(hptEffPP, "pp");
   leg2->Draw();
-  drawText(Form("#Upsilon(%dS),  p_{T}^{#mu} > 4GeV/c",state),0.25,0.87,1,15); 
+  drawText(Form("#Upsilon(%dS),  p_{T}^{#mu} > 4GeV/c",state),0.25,0.87,1,15);
   jumSun(0,1,30,1);
-  
+
   c_eff_pt->SaveAs(Form("eff_vs_pt_%ds.pdf",state));
 
   // Efficiency Rap
@@ -283,102 +339,61 @@ void getEfficiencyUpsilon(int state = 1, bool useDataWeight=true) {  // 1S, 2S, 
   leg3->AddEntry(hrapEffPP, "pp");
   leg3->Draw();
   jumSun(0,1,30,1);
-  drawText(Form("#Upsilon(%dS),  p_{T}^{#mu} > 4GeV/c",state),0.25,0.87,1,15); 
+  drawText(Form("#Upsilon(%dS),  p_{T}^{#mu} > 4GeV/c",state),0.25,0.87,1,15);
   c_eff_rap->SaveAs(Form("eff_vs_rap_%ds.pdf",state));
- 
-  // Centrality Efficiency 
+
+  // Centrality Efficiency
   TCanvas* c_eff_cent =  new TCanvas("c_eff_cent","",400,400);
   TH1D* hcentEffAA;
-  TH1D* hcentEffPP;
-  TH1D* hcentEffAA_int;
-  hcentEffAA_int = (TH1D*) hcentintRecoAA->Clone("hcentEffAA_int");
-  hcentEffAA_int -> Divide(hcentintGenAA);
-  hcentEffAA_int -> SetAxisRange(0,1.2,"Y");
-  hcentEffAA_int -> SetYTitle("efficiency");
-  hcentEffPP = (TH1D*)hcentRecoPP->Clone("hcentEffPP");
-  hcentEffPP ->Divide(hcentGenPP);
-  hcentEffPP ->SetMarkerStyle(24);
-  hcentEffPP ->SetLineStyle(2);
+  TH1D* hcentintEffPP;
+  TH1D* hcentintEffAA;
+  hcentintEffAA = (TH1D*) hcentintRecoAA->Clone("hcentintEffAA");
+  hcentintEffAA -> Divide(hcentintGenAA);
+  hcentintEffAA -> SetAxisRange(0,1.2,"Y");
+  hcentintEffAA -> SetYTitle("efficiency");
+  hcentintEffPP = (TH1D*)hcentintRecoPP->Clone("hcentintEffPP");
+  hcentintEffPP ->Divide(hcentintGenPP);
+  hcentintEffPP ->SetMarkerStyle(24);
+  hcentintEffPP ->SetLineStyle(2);
   hcentEffAA = (TH1D*)hcentRecoAA->Clone("hcentEffAA");
   hcentEffAA ->Divide(hcentGenAA);
   hcentEffAA ->SetAxisRange(0,1.2,"Y");
   hcentEffAA ->SetYTitle("efficiency");
   hcentEffAA ->Draw();
-  hcentEffAA_int -> Draw("same hist");
-  hcentEffPP ->Draw("same hist");
+  hcentintEffAA -> Draw("same hist");
+  hcentintEffPP ->Draw("same hist");
 
   TLegend* leg4 = new TLegend(0.4046176,0.3500982,0.8492568,0.5304435,NULL,"brNDC");
   easyLeg(leg4,"|y| < 2.4");
-  leg4->AddEntry(hcentEffPP, "pp","l");
-  leg4->AddEntry(hcentEffAA_int, "PbPb (0-100%)","l");
+  leg4->AddEntry(hcentintEffPP, "pp","l");
+  leg4->AddEntry(hcentintEffAA, "PbPb (0-100%)","l");
   leg4->AddEntry(hcentEffAA, "PbPb","pl");
   leg4->Draw();
-  drawText(Form("#Upsilon(%dS),  p_{T}^{#mu} > 4GeV/c",state),0.25,0.87,1,15); 
+  drawText(Form("#Upsilon(%dS),  p_{T}^{#mu} > 4GeV/c",state),0.25,0.87,1,15);
   jumSun(0,1,200,1);
   c_eff_cent->SaveAs(Form("eff_vs_cent_%ds.pdf",state));
 
-  
-  TFile *fout = new TFile(Form("efficiency_ups%ds_useDataWeight%d.root",state,useDataWeight),"recreate");
-  hptGenPP->Write();
-  hptRecoPP->Write();
-  hptEffPP->Write();
-  hptGenAA->Write();
-  hptRecoAA->Write();
-  hptEffAA->Write();
-  
-  hrapGenPP->Write();
-  hrapRecoPP->Write();
-  hrapEffPP->Write();
-  hrapGenAA->Write();
-  hrapRecoAA->Write();
-  hrapEffAA->Write();
 
-  hcentEffPP->Write();
+  TFile *fout = new TFile(Form("efficiency_ups%ds_useDataPtWeight%d_tnpWeight%d.root",state,useDataWeight,useTnpWeigt),"recreate");
+  //  hptGenPP->Write();
+  //  hptRecoPP->Write();
+  //  hptGenAA->Write();
+  //  hptRecoAA->Write();
+  //  hrapGenPP->Write();
+  //  hrapRecoPP->Write();
+  //  hrapGenAA->Write();
+  //  hrapRecoAA->Write();
+  hptEffPP->Write();
+  hptEffAA->Write();
+  hrapEffPP->Write();
+  hrapEffAA->Write();
+  hcentintEffPP->Write();
   hcentEffAA->Write();
-  hcentEffAA_int->Write();
+  hcentintEffAA->Write();
+  
   fout->Close();
 
 
-}
-
-void setupMultiTreeTool( multiTreeUtil* mt, int UpsState, bool isGen) { 
-  TString treeName = "mm";
-  if ( isGen) treeName = "mmGen";
-
-  if ( UpsState == kPPMCUps1S) {    
-    mt->addFile(
-		//"../skimmedFiles/yskimPP_MC_Ups1S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20168142115_3c54df0419c4813e2d7256dc8952ac699405d027.root"
-		"../skimmedFilesWeight2/yskimPP_MC_Ups1S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20161281226_.root"
-		,treeName,""); } 
-  else if ( UpsState == kPPMCUps2S) { 
-    mt->addFile(
-		//		"../skimmedFiles/yskimPP_MC_Ups2S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20168142117_3c54df0419c4813e2d7256dc8952ac699405d027.root"
-		"../skimmedFilesWeight2/yskimPP_MC_Ups2S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20161281228_.root"
-		,treeName,"");  }
-  else if ( UpsState == kPPMCUps3S) { 
-    mt->addFile(
-		//		"../skimmedFiles/yskimPP_MC_Ups3S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20168142119_3c54df0419c4813e2d7256dc8952ac699405d027.root"
-		"../skimmedFilesWeight2/yskimPP_MC_Ups3S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20161281230_.root"
-		,treeName,"");  }
   
-  else if ( UpsState == kAAMCUps1S) { 
-    mt->addFile(
-		//		"../skimmedFiles/yskimAA_MC_Ups1S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20168142122_3c54df0419c4813e2d7256dc8952ac699405d027.root",
-		"../skimmedFilesWeight2/yskimAA_MC_Ups1S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20161281233_.root"
-		,treeName,"");  } // Weighted
-  else if ( UpsState == kAAMCUps2S) { 
-    mt->addFile(
-		//		"../skimmedFiles/yskimAA_MC_Ups2S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20168142123_3c54df0419c4813e2d7256dc8952ac699405d027.root"
-		"../skimmedFilesWeight2/yskimAA_MC_Ups2S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20161281234_.root"
-		,treeName,"");
-  }
-  else if ( UpsState == kAAMCUps3S) {
-    mt->addFile(
-		//		"../skimmedFiles/yskimAA_MC_Ups3S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20168142124_3c54df0419c4813e2d7256dc8952ac699405d027.root"
-		"../skimmedFilesWeight2/yskimAA_MC_Ups3S_Trig-L1DoubleMu0_OpSign_EP-OppositeHF_20161281235_.root"
-		,treeName,"");
-  }
   
 }
-
-
--- efficiency/runEff.sh
+++ efficiency/runEff.sh
@@ -1,6 +1,8 @@
-root -l -b -q 'getEfficiencyUpsilon.C+(1,0)'
-root -l -b -q 'getEfficiencyUpsilon.C+(2,0)'
-root -l -b -q 'getEfficiencyUpsilon.C+(3,0)'
-root -l -b -q 'getEfficiencyUpsilon.C+(1,1)'
-root -l -b -q 'getEfficiencyUpsilon.C+(2,1)'
-root -l -b -q 'getEfficiencyUpsilon.C+(3,1)'
+#void getEfficiencyUpsilon(int state = 1, bool useDataWeight=true, bool useTnpWeigt=false) {  // 1S, 2S, 3S
+
+root -l -b -q 'getEfficiencyUpsilon.C+(1,0,0)'
+root -l -b -q 'getEfficiencyUpsilon.C+(2,0,0)'
+root -l -b -q 'getEfficiencyUpsilon.C+(3,0,0)'
+root -l -b -q 'getEfficiencyUpsilon.C+(1,1,0)'
+root -l -b -q 'getEfficiencyUpsilon.C+(2,1,0)'
+root -l -b -q 'getEfficiencyUpsilon.C+(3,1,0)'
