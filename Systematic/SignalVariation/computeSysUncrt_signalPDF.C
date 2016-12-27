fa2f5ac4b011b8fdd2926f83f9b7b5fddf985c3a
JaeBeom Park <pjwinnetou@gmail.com>
2016-12-27 16:55:20 +0900

    signal sys compute up

--- Systematic/SignalVariation/computeSysUncrt_signalPDF.C
+++ Systematic/SignalVariation/computeSysUncrt_signalPDF.C
@@ -12,6 +12,7 @@ using namespace std;
 const int nVar = 5 ;     // 0 : nominal value
 
 double getRMS(TString fileName = "DR_SignalSys/", int state=1);
+double getMaxDev(TString fileName = "DR_SignalSys/", int state=1);
 double getRMS31(TString fileName = "DR_SignalSys/");
 double getUncVar(TString ppfilename = "DR_SignalSys/", TString pbpbfilename = "DR_SignalSys/", int varnum = 1);
 
@@ -48,17 +49,17 @@ void computeSysUncrt_signalPDF(int state=1)
   cout << endl;
   cout << " CENTRALITY DEPENDENCE===================== " << endl;
   cout << " pp :   " << endl; 
-  pp = getRMS("fitresults_upsilon_DoubleCB_PP_DATA_pt0.0-30.0_y0.0-2.4_muPt4.0"); // pp.  pT/y integrated
+  pp = getMaxDev("fitresults_upsilon_DoubleCB_PP_DATA_pt0.0-30.0_y0.0-2.4_muPt4.0"); // pp.  pT/y integrated
   cout << " AA :   " << endl; 
   for ( int icent = 0 ; icent<= nCentBins ; icent++ ) 
   {
     if(icent == 0) cout << "Centrality 0 - 200" << endl;
     else cout << Form("Centrality  %d - %d", centBin[icent-1], centBin[icent]) << endl;
-    if(icent == 0) aa = getRMS("fitresults_upsilon_DoubleCB_AA_DATA_pt0.0-30.0_y0.0-2.4_muPt4.0_centrality0-200_dphiEp_0.00PI_100.00PI");
+    if(icent == 0) aa = getMaxDev("fitresults_upsilon_DoubleCB_AA_DATA_pt0.0-30.0_y0.0-2.4_muPt4.0_centrality0-200_dphiEp_0.00PI_100.00PI");
     else 
     {
-      if(centBin[icent] <= 60) aa = getRMS(Form("fitresults_upsilon_DoubleCB_AA_DATA_pt0.0-30.0_y0.0-2.4_muPt4.0_centrality%d-%d_dphiEp_0.00PI_100.00PI", centBin[icent-1], centBin[icent]),state );
-      else if(centBin[icent] > 60) aa = getRMS(Form("fitresults_upsilon_DoubleCB_AA_DATA_PeriL1_pt0.0-30.0_y0.0-2.4_muPt4.0_centrality%d-%d_dphiEp_0.00PI_100.00PI", centBin[icent-1], centBin[icent]),state );
+      if(centBin[icent] <= 60) aa = getMaxDev(Form("fitresults_upsilon_DoubleCB_AA_DATA_pt0.0-30.0_y0.0-2.4_muPt4.0_centrality%d-%d_dphiEp_0.00PI_100.00PI", centBin[icent-1], centBin[icent]),state );
+      else if(centBin[icent] > 60) aa = getMaxDev(Form("fitresults_upsilon_DoubleCB_AA_DATA_PeriL1_pt0.0-30.0_y0.0-2.4_muPt4.0_centrality%d-%d_dphiEp_0.00PI_100.00PI", centBin[icent-1], centBin[icent]),state );
     }
     finalUnc = sqrt( pp*pp + aa*aa );
     cout << "Unc. of double ratio  " << 0.00001* int(10000000*finalUnc) << "%%" << endl;
@@ -69,10 +70,11 @@ void computeSysUncrt_signalPDF(int state=1)
   cout << " pT DEPENDENCE===================== " << endl;
   cout << " pp :   " << endl; 
   cout << " AA :   " << endl; 
-  for ( int ipt = 1 ; ipt<= nPtBins ; ipt++ ) {
+  for ( int ipt = 1 ; ipt<= nPtBins ; ipt++ ) 
+  {
     cout << Form("pT : %.1f - %.1f GeV/c", (float)ptBin[ipt-1], ptBin[ipt]) << endl;
-    pp = getRMS(Form("fitresults_upsilon_DoubleCB_PP_DATA_pt%.1f-%.1f_y0.0-2.4_muPt4.0", ptBin[ipt-1], ptBin[ipt]), state); // pp
-    aa = getRMS(Form("fitresults_upsilon_DoubleCB_AA_DATA_pt%.1f-%.1f_y0.0-2.4_muPt4.0_centrality0-200_dphiEp_0.00PI_100.00PI", ptBin[ipt-1], ptBin[ipt] ),state ); // PbPb
+    pp = getMaxDev(Form("fitresults_upsilon_DoubleCB_PP_DATA_pt%.1f-%.1f_y0.0-2.4_muPt4.0", ptBin[ipt-1], ptBin[ipt]), state); // pp
+    aa = getMaxDev(Form("fitresults_upsilon_DoubleCB_AA_DATA_pt%.1f-%.1f_y0.0-2.4_muPt4.0_centrality0-200_dphiEp_0.00PI_100.00PI", ptBin[ipt-1], ptBin[ipt] ),state ); // PbPb
     finalUnc = sqrt( pp*pp + aa*aa );
     cout << "Unc. of double ratio  " << 1.000000* double(100*finalUnc) << "%%" << endl;
     cout << endl;
@@ -82,10 +84,11 @@ void computeSysUncrt_signalPDF(int state=1)
   cout << " y DEPENDENCE===================== " << endl;
   cout << " pp :   " << endl; 
   cout << " AA :   " << endl; 
-  for ( int iy = 1 ; iy<= nYBins ; iy++ ) {
+  for ( int iy = 1 ; iy<= nYBins ; iy++ ) 
+  {
     cout << Form("y : %.1f - %.1f", (float)yBin[iy-1], yBin[iy]) << endl;
-    pp = getRMS(Form("fitresults_upsilon_DoubleCB_PP_DATA_pt0.0-30.0_y%.1f-%.1f_muPt4.0", yBin[iy-1], yBin[iy]),state ); // pp
-    aa = getRMS(Form("fitresults_upsilon_DoubleCB_AA_DATA_pt0.0-30.0_y%.1f-%.1f_muPt4.0_centrality0-200_dphiEp_0.00PI_100.00PI", yBin[iy-1], yBin[iy]),state ); // PbPb
+    pp = getMaxDev(Form("fitresults_upsilon_DoubleCB_PP_DATA_pt0.0-30.0_y%.1f-%.1f_muPt4.0", yBin[iy-1], yBin[iy]),state ); // pp
+    aa = getMaxDev(Form("fitresults_upsilon_DoubleCB_AA_DATA_pt0.0-30.0_y%.1f-%.1f_muPt4.0_centrality0-200_dphiEp_0.00PI_100.00PI", yBin[iy-1], yBin[iy]),state ); // PbPb
     finalUnc = sqrt( pp*pp + aa*aa );
     cout << "Unc. of double ratio  " << 1.000000* double(100*finalUnc) << "%%" << endl;
   }
@@ -147,18 +150,48 @@ double getRMS(TString fileName , int state)
     relVar[ivar] = yield[ivar]/yield[0] -1 ;
     if ( ivar>0) 
       cout << " & " << Form("%.6f",100*double(relVar[ivar])) << "%%";
-      //cout << " & " << 0.1 * double(1000*relVar[ivar]) << "%%";
   }
-  
+   
   quadSum=0;
   for ( int ivar=0 ; ivar<=nVar ; ivar++) 
   {
     quadSum = quadSum + relVar[ivar]*relVar[ivar] ;
   }
-
+   
   quadSum = quadSum/(double)nVar;
   double theRMS = sqrt(quadSum);
   cout << " & " << Form("%.6f",100*double(theRMS)) << endl;
   return theRMS;
+}
+
+double getMaxDev(TString fileName , int state) 
+{
+  double yield[nVar+1];
+  double relVar[nVar+1];   // relative variance
+  double MaxDev;
+
+  TString fin;
+  cout << "Var1 - Var5 : " ;
+  for ( int ivar=0 ; ivar<=nVar ; ivar++) 
+  { 
+    if(ivar ==0) fin = Form("%s.root",fileName.Data());
+    else fin = Form("Sys_SignalVar_%s_%d.root", fileName.Data(), ivar);
+    TFile* f1 = new TFile(fin.Data() );
+    TH1D* h = (TH1D*)f1->Get("fitResults");
+    yield[ivar] = (double)h->GetBinContent(state);
+    relVar[ivar] = yield[ivar]/yield[0] -1 ;
+    if ( ivar>0) 
+      cout << " & " << Form("%.6f",100*double(relVar[ivar])) << "%%";
+  }
+  
+  MaxDev = relVar[1]*relVar[1];
+    
+  for ( int ivar=1 ; ivar<=nVar ; ivar++) 
+  {
+    if(MaxDev >= relVar[ivar]*relVar[ivar]) {MaxDev = relVar[ivar]*relVar[ivar];}
+  }
+  
+  cout << " & " << Form("%.6f",100*double(MaxDev)) << endl;
+  return MaxDev;
 }
 
