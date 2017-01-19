b9af501e21fb2bdd01b4a1e7a3ef023fe5b8575c
SongkyoLee <songkyo.lee@cern.ch>
2017-01-20 05:26:23 +0900

    draw signal parameters

---
+++ shell_draw_sigparam.sh
@@ -0,0 +1,16 @@
+#int draw_sigparam_pt(TString szAA = "PP", int states =1)
+### pt ###
+root -l -b -q 'draw_sigparam_pt.C+("PP",1)'
+root -l -b -q 'draw_sigparam_pt.C+("PP",2)'
+root -l -b -q 'draw_sigparam_pt.C+("PP",3)'
+root -l -b -q 'draw_sigparam_pt.C+("AA",1)'
+root -l -b -q 'draw_sigparam_pt.C+("AA",2)'
+root -l -b -q 'draw_sigparam_pt.C+("AA",3)'
+### rap ###
+#root -l -b -q 'draw_sigparam_rap.C+("PP",1)'
+#root -l -b -q 'draw_sigparam_rap.C+("PP",2)'
+#root -l -b -q 'draw_sigparam_rap.C+("PP",3)'
+#root -l -b -q 'draw_sigparam_rap.C+("AA",1)'
+#root -l -b -q 'draw_sigparam_rap.C+("AA",2)'
+#root -l -b -q 'draw_sigparam_rap.C+("AA",3)'
+
