d900254333b3913b8dfe14cf316f492443842339
Yongsun Kim <yongsunkim@Yongsuns-MacBook-Pro.local>
2017-01-26 23:33:13 -0600

    full uncertainty made

--- efficiency/runEff.sh
+++ efficiency/runEff.sh
@@ -21,23 +21,35 @@ do
     root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,0,0,-100,-100)'
     root -l -b -q 'getEfficiencyUpsilon.C+('$state',0,-100,-100,-100,-100)'  #Pure MC
     root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,-100,-100,-100,-100)'
-
 done
 
 # SYSTEMATICS
-
+#    int trgIdx, trkIdx, muIdx, staIdx
 for state in 1 2 3
 do
-#    root -l -b -q 'getEfficiencyUpsilon.C('$state',1,1,-1)'
-#    root -l -b -q 'getEfficiencyUpsilon.C('$state',1,1,-2)'
-#    root -l -b -q 'getEfficiencyUpsilon.C('$state',1,1,-10)'
-#    root -l -b -q 'getEfficiencyUpsilon.C('$state',1,1,200)'
-#    root -l -b -q 'getEfficiencyUpsilon.C('$state',1,1,300)'
-#    for (( i=1 ; i<=100 ; i++ ))
- #   do
- #       root -l -b -q 'getEfficiencyUpsilon.C('$state',1,1,'$i')'
-#    done
-done
+    root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,-1,0,-100,-100)'   
+    root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,-2,0,-100,-100)'   
+    
+    root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,0,-1,-100,-100)'   
+    root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,0,-2,-100,-100)'   
+
+    root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,0,0,-1,-100)'   
+    root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,0,0,-2,-100)'   
+
+    root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,0,0,-100,-1)'   
+    root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,0,0,-100,-2)'   
+
+    root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,-10,0,-100,-100)'   
+
+    for (( i=1 ; i<=100 ; i++ ))
+    do
+        root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,'$i',0,-100,-100)'   # stat for trigger
+        root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,0,0,'$i',-100)'     # stat for muid
+        root -l -b -q 'getEfficiencyUpsilon.C+('$state',1,0,0,-100,'$i')'     # stat for muid
+    done
+
+
+done    
 
 
 
