b863b8d8f5ac09e74a19fbdd4c31f44006c1c3f2
JaeBeom Park <pjwinnetou@gmail.com>
2017-02-03 02:49:46 +0900

    sh

--- finalResults/run_massPlot.sh
+++ finalResults/run_massPlot.sh
@@ -1,3 +1,5 @@
 root -l -q -b doFitUpsilon_Data_pas_AA_pt02_y0408.C+ 
 root -l -q -b doFitUpsilon_Data_pas_PP_pt02.C+ 
 root -l -q -b doFitUpsilon_Data_pas_overlay.C+ 
+
+rm -f *.so *.d *.pcm
--- finalResults/run_strickland.sh
+++ finalResults/run_strickland.sh
@@ -1,3 +1,7 @@
 root -l -b -q strickland_RAA_pt_isArrow.C+ 
 root -l -b -q strickland_RAA_rap_isArrow.C+ 
-root -l -b -q strickland_RAA_cent.C+
+root -l -b -q strickland_RAA_3S_cent.C+
+root -l -b -q strickland_RAA_3S_pt.C+
+root -l -b -q strickland_RAA_3S_rap.C+
+
+rm -f *.so *.d *.pcm
